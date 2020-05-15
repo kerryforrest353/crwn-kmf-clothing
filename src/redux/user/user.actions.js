5f0a5cc6af16690b2a4be0b77601015d099bfd37
Kerry Forrest <michael.k.forrest@gmail.com>
2020-05-15 11:22:10 -0700

    add addItem and removeItem functionality to checkout item component

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,12 @@
 import { UserActionTypes } from "./user.types";
+import CartActionTypes from "../cart/cart.types";
 
 export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
 });
